42053e4e9b3e956e3bff818e09765e45a61351d5
LIUKEyan1110 <652169700@qq.com>
2021-02-24 13:40:15 +0800

    lky

--- Doily-android-client/Doily/app/src/main/java/com/selfmanage/doily/activity/accountBook.java
+++ Doily-android-client/Doily/app/src/main/java/com/selfmanage/doily/activity/accountBook.java
@@ -1,15 +1,37 @@
 package com.selfmanage.doily.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import com.selfmanage.doily.fragment.*;
 
 import com.selfmanage.doily.R;
 
 public class accountBook extends AppCompatActivity {
+    private ImageView back_club=null;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.accountbook);
+        findView();
+        setListener();
+    }
+
+    private void setListener() {
+        back_club.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                com.selfmanage.doily.activity.accountBook.this.finish();
+            }
+        });
+    }
+
+    private void findView() {
+        back_club=findViewById(R.id.back_club);
+
     }
 }
--- Doily-android-client/Doily/app/src/main/java/com/selfmanage/doily/activity/lifeDaily.java
+++ Doily-android-client/Doily/app/src/main/java/com/selfmanage/doily/activity/lifeDaily.java
@@ -3,13 +3,32 @@ package com.selfmanage.doily.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.selfmanage.doily.R;
 
 public class lifeDaily extends AppCompatActivity{
+    private ImageView back_club=null;
+    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lifedaliy);
+        setContentView(R.layout.concentration);
+        getSupportActionBar().hide();
+        findView();
+        setListener();
+    }
+    private void setListener() {
+        back_club.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                com.selfmanage.doily.activity.lifeDaily.this.finish();
+            }
+        });
+    }
+
+    private void findView() {
+        back_club=findViewById(R.id.back_club);
 
     }
 }
